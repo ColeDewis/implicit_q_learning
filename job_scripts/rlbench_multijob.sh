d5c2565caa2a150d87e2da0ec2c45cd50905d0ed
Serg <serg@gmail.com>
2025-11-25 16:57:23 -0700

    hopefully can specify port

--- job_scripts/rlbench_multijob.sh
+++ job_scripts/rlbench_multijob.sh
@@ -61,6 +61,7 @@ mkdir -p $LOG_DIR
 # Training loop for multiple seeds
 for ((i=0; i<STEP_SIZE; i++)); do
     SEED=$((SLURM_ARRAY_TASK_ID + i))
+    PORT=$((i+5000))
     SESSION_NAME="pair_seed_${SEED}"
     echo "Starting Tmux session: $SESSION_NAME"
 
@@ -73,7 +74,7 @@ for ((i=0; i<STEP_SIZE; i++)); do
     tmux send-keys -t ${SESSION_NAME}:0.0 "cd $SLURM_TMPDIR" C-m
     tmux send-keys -t ${SESSION_NAME}:0.0 "source .venv/bin/activate" C-m
     tmux send-keys -t ${SESSION_NAME}:0.0 "$setup_iql_cmds" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.0 "python $path/train_offline.py --env_name=$ENV_NAME --config=$path/configs/${CONFIG_NAME} --eval_episodes=100 --eval_interval=${EVAL_INTERVAL} --seed=$SEED > $LOG_TRAIN 2>&1" C-m
+    tmux send-keys -t ${SESSION_NAME}:0.0 "python $path/train_offline.py --env_name=$ENV_NAME --config=$path/configs/${CONFIG_NAME} --eval_episodes=100 --eval_interval=${EVAL_INTERVAL} --seed=$SEED --port=$PORT > $LOG_TRAIN 2>&1" C-m
     tmux send-keys -t ${SESSION_NAME}:0.0 "cp ./tmp/IQL_${SEED}.txt $RESULTS_DIR" C-m
 
 
@@ -82,7 +83,7 @@ for ((i=0; i<STEP_SIZE; i++)); do
     tmux send-keys -t ${SESSION_NAME}:0.1 "cd $SLURM_TMPDIR" C-m
     tmux send-keys -t ${SESSION_NAME}:0.1 "source .venv_rlbench/bin/activate" C-m
     tmux send-keys -t ${SESSION_NAME}:0.1 "$setup_rlbench_cmds" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.1 "xvfb-run -a python $path/../RLBench/env_server.py > $LOG_PARTNER 2>&1" C-m
+    tmux send-keys -t ${SESSION_NAME}:0.1 "xvfb-run -a python $path/../RLBench/env_server.py --port=$PORT > $LOG_PARTNER 2>&1" C-m
 done
 
 echo "Waiting for Tmux sessions to complete..."
