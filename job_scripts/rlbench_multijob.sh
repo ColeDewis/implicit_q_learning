98b19727bec530fe4b8960436dfb0d1e427e02d1
Sergey <sergey.khl.dev@gmail.com>
2025-11-26 00:01:39 -0700

    2 windows not 4

--- job_scripts/rlbench_multijob.sh
+++ job_scripts/rlbench_multijob.sh
@@ -66,19 +66,19 @@ for ((i=0; i<STEP_SIZE; i++)); do
     # tmux will have 2 instances per session
     # Instance 1 will have iql repo:
     tmux new-session -d -s $SESSION_NAME
-    tmux send-keys -t ${SESSION_NAME}:0.0 "cd $SLURM_TMPDIR" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.0 "source .venv/bin/activate" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.0 "$setup_iql_cmds" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.0 "python $path/train_offline.py --env_name=$ENV_NAME --config=$path/configs/${CONFIG_NAME} --eval_episodes=100 --eval_interval=${EVAL_INTERVAL} --seed=$SEED --port=$PORT" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.0 "cp ./tmp/IQL_${SEED}.txt $RESULTS_DIR" C-m
+    tmux send-keys -t ${SESSION_NAME}:0 "cd $SLURM_TMPDIR" C-m
+    tmux send-keys -t ${SESSION_NAME}:0 "source .venv/bin/activate" C-m
+    tmux send-keys -t ${SESSION_NAME}:0 "$setup_iql_cmds" C-m
+    tmux send-keys -t ${SESSION_NAME}:0 "python $path/train_offline.py --env_name=$ENV_NAME --config=$path/configs/${CONFIG_NAME} --eval_episodes=100 --eval_interval=${EVAL_INTERVAL} --seed=$SEED --port=$PORT" C-m
+    tmux send-keys -t ${SESSION_NAME}:0 "cp ./tmp/IQL_${SEED}.txt $RESULTS_DIR" C-m
 
 
     # Instance 2 will have rlbench repo:
-    tmux split-window -h -t ${SESSION_NAME}:0
-    tmux send-keys -t ${SESSION_NAME}:0.1 "cd $SLURM_TMPDIR" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.1 "source .venv_rlbench/bin/activate" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.1 "$setup_rlbench_cmds" C-m
-    tmux send-keys -t ${SESSION_NAME}:0.1 "xvfb-run -a python $path/../RLBench/env_server.py --port=$PORT" C-m
+    tmux new-window -h -t ${SESSION_NAME}:0
+    tmux send-keys -t ${SESSION_NAME}:1 "cd $SLURM_TMPDIR" C-m
+    tmux send-keys -t ${SESSION_NAME}:1 "source .venv_rlbench/bin/activate" C-m
+    tmux send-keys -t ${SESSION_NAME}:1 "$setup_rlbench_cmds" C-m
+    tmux send-keys -t ${SESSION_NAME}:1 "xvfb-run -a python $path/../RLBench/env_server.py --port=$PORT" C-m
 done
 
 echo "Waiting for Tmux sessions to complete..."
