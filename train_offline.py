dbfc6ae7b96bdf6c842322f394d52b77b4d12403
Serg <serg@gmail.com>
2025-11-28 18:37:49 -0700

    pls

--- train_offline.py
+++ train_offline.py
@@ -178,13 +178,6 @@ def main(_):
     eval_returns = []
     if FLAGS.dummy:
         eval_returns = [(-1, -10)]
-    np.savetxt(
-        os.path.join(FLAGS.save_dir, save_file_name),
-        [(-1, -10)],
-        fmt=["%d", "%.1f"],
-    )
-    env.close()
-    return
     for i in tqdm.tqdm(
         range(1, FLAGS.max_steps + 1), smoothing=0.1, disable=not FLAGS.tqdm
     ):
